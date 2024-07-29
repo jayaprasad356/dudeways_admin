036d4fc907aefc23d34dcffea140aabd0d98da13
Akraja712 <ashokkumar7hitter@gmail.com>
2024-07-29 16:24:02 +0530

    index.blade.php

--- resources/views/trips/index.blade.php
+++ resources/views/trips/index.blade.php
@@ -207,6 +207,36 @@
             }
         });
     </script>
+<script>
+  $(document).on('click', '.btn-delete', function () {
+                $this = $(this);
+                const swalWithBootstrapButtons = Swal.mixin({
+                    customClass: {
+                        confirmButton: 'btn btn-success',
+                        cancelButton: 'btn btn-danger'
+                    },
+                    buttonsStyling: false
+                })
+
+                swalWithBootstrapButtons.fire({
+                    title: 'Are you sure?',
+                    text: "Do you really want to delete this customer?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'Yes, delete it!',
+                    cancelButtonText: 'No',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.value) {
+                        $.post($this.data('url'), {_method: 'DELETE', _token: '{{csrf_token()}}'}, function (res) {
+                            $this.closest('tr').fadeOut(500, function () {
+                                $(this).remove();
+                            })
+                        })
+                    }
+                })
+            });
+</script>
 
     <script>
         $(document).ready(function () {
