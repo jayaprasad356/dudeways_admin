06b5f0f2dc14f1661ff948e462f2a9031674fa0c
Akraja712 <ashokkumar7hitter@gmail.com>
2024-10-15 12:32:26 +0530

    AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2475,15 +2475,15 @@ public function add_chat(Request $request)
         protected function sendNotificationsToUser($chat_user_id, $message)
             {
                 $user = Users::find($chat_user_id);
+
                 
                 if ($user && $user->online_status == 0) {
-                    $sender = auth()->user(); 
                     
                     // Send notification via OneSignal
                     $this->oneSignalClient->sendNotificationToExternalUser(
                         $message,
                         $chat_user_id,
-                        $url = 'https://www.dudeways.com/path/to/userid=' . strval($chat_user_id) . '&chatid=' . strval($user->user_id) . '&senderName=' . urlencode($sender->name) . '&receiverName=' . urlencode($user->name) . '&senderUniqueName=' . urlencode($sender->unique_name) . '&receiverName=' . urlencode($user->name),
+                        $url = 'https://www.dudeways.com/path/to/userid=' . strval($chat_user_id) . '&chatid=' . strval($user->user_id) . '&senderName=' . urlencode($user->name) . '&receiverName=' . urlencode($user->name) . '&senderUniqueName=' . urlencode($user->unique_name) . '&receiverName=' . urlencode($user->name),
                         $data = null,
                         $buttons = null,
                         $schedule = null
